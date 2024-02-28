e82ff93b89190f6186118eae086dccfdd4807003
Kyaw Thiha <kth4dev@gmail.com>
2024-02-28 12:26:48 +0630

    first commit

---
+++ WindowsServiceTutorial/Service1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using Timer = System.Timers.Timer;
+
+namespace WindowsServiceTutorial
+{
+    public partial class Service1 : ServiceBase
+    {
+        Timer timer = new Timer();
+
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        
+        protected override void OnStart(string[] args)
+        {
+            WriteToFile("Service is started at " + DateTime.Now);
+            timer.Elapsed += new ElapsedEventHandler(OnElapsedTime);   
+            timer.Interval = 5000;
+            timer.Enabled = true;
+        }
+
+        protected override void OnStop()
+        {
+            WriteToFile("Service is stopped at " + DateTime.Now);
+        }
+
+        private void OnElapsedTime(object sender, ElapsedEventArgs e)
+        {
+            WriteToFile($"Service was run at : {DateTime.Now}");
+        }
+
+        public void WriteToFile(string message)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "\\Logs";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            string filepath = AppDomain.CurrentDomain.BaseDirectory + "\\Logs\\ServiceLog_" + DateTime.Now.Date.ToShortDateString().Replace('/','_') + ".txt";
+            if(!File.Exists(filepath))
+            {
+                using (StreamWriter sw = File.CreateText(filepath))
+                {
+                    sw.WriteLine(message);
+                }
+            }
+            else
+            {
+
+                using (StreamWriter sw = File.AppendText(filepath))
+                {
+                    sw.WriteLine(message);
+                }
+            }
+        }
+    }
+}
